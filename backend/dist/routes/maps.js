be79218e3c95c1792685404f130bb8743931716a
Marcelo <marcelo.smarques7@gmail.com>
2024-11-26 11:53:29 -0300

    Stop tracking dist folder

--- backend/dist/routes/maps.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const maps_1 = require("../handlers/maps");
-const mapsRouter = (0, express_1.Router)();
-mapsRouter.post("/image", maps_1.getMapUrl);
-exports.default = mapsRouter;
