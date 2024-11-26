2d6c5a6f6392e772adb7fef34d4b58b470994ceb
Marcelo <marcelo.smarques7@gmail.com>
2024-11-26 11:53:29 -0300

    Stop tracking dist folder

--- backend/dist/services/driverExistById.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.driverExistById = driverExistById;
-function driverExistById(id) {
-    const validIds = [1, 2, 3];
-    return validIds.includes(id);
-}
